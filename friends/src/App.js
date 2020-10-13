02d064dc76295ec05cd7d140aa9a012b6b25fbf6
Sahar Jafari <shr.jafari@gmail.com>
2020-10-13 14:00:30 -0700

    Setup the app + axiosWithAuth

---
+++ friends/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { axiosWithAuth } from "./utils/axiosWithAuth";
+import "./App.css";
+
+function App() {
+  return <div className="App">Hello world!</div>;
+}
+
+export default App;
