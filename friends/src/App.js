96e7244450cb156db558873389250bd30b05c7c3
Sahar Jafari <shr.jafari@gmail.com>
2020-10-13 21:15:12 -0700

    Login UI and func

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import { axiosWithAuth } from "./utils/axiosWithAuth";
+// import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
+// import Friends from "./components/Friends";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* <PrivateRoute exact path="/protected" component={Friends} /> */}
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
